12244e2f8f955d5e5b2021771ca5bc53c1d269a1
roqkfchqh <plmokn48@daum.net>
2025-02-10 02:23:17 +0900

    refactor: 카프카 포기

--- src/main/java/com/example/acfac/common/AbstractRequestProcessor.java
+++ src/main/java/com/example/acfac/common/AbstractRequestProcessor.java
@@ -1,6 +1,5 @@
 package com.example.acfac.common;
 
-import com.example.acfac.kafka.KafkaRequestProducer;
 import com.example.acfac.rest.UserRequestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +14,6 @@ import reactor.core.publisher.Mono;
 public abstract class AbstractRequestProcessor {
 
     protected final LoadBalancer loadBalancer;
-    protected final KafkaRequestProducer kafkaRequestProducer;
     protected final WebClient webClient;
     protected final HealthCheckService healthCheckService;
 
@@ -25,19 +23,8 @@ public abstract class AbstractRequestProcessor {
     public Mono<String> processRequest(UserRequestDto dto, String clientIp) {
         String serverUrl = loadBalancer.getNextServer(healthCheckService.getHealthyServers());
 
-        // Kafka 로깅 처리 TODO: 이 부분도 httpMethod 따라 로깅 다르게 처리 가능..
-        Mono<Void> kafkaLogging = Mono.fromRunnable(() -> {
-            try {
-                kafkaRequestProducer.logRequest(clientIp, serverUrl);
-            } catch (Exception e) {
-                log.error("Kafka 서버 오류: {}", e.getMessage());
-            }
-        });
-
         // 하위 클래스에서 HTTP 요청 전략 실행
-        return kafkaLogging.then(
-            executeRequest(serverUrl, dto.json())
-        );
+        return executeRequest(serverUrl, dto.json());
     }
 
     /**
